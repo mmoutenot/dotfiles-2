9c3097cb6dff4c0e1933333840015cd1fa90a855
Vojtech Balak <vojtech.balak@internet-handel.cz>
2017-05-06 17:46:31 +0200

    Deleted some unused config

--- .i3/i3status_wrapper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-i3status -c ~/.i3/i3status.conf | while :
-do
-    read line
-    layout=$(xkb-switch)
-    brightness=$(cat /sys/class/backlight/acpi_video0/brightness)
-    echo "  $layout |  ${brightness}% | $line" || exit 1
-done
